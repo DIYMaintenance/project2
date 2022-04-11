7cbf818d3149d299f4c0a26d63a27c47a2db520a
DIYMaintenance <98708577+DIYMaintenance@users.noreply.github.com>
2022-04-11 03:12:09 +0300

    Fix
    Korjaus hakuun. Löytää nyt osittaisella nimellä elokuvan.

--- myJavascript.js
+++ myJavascript.js
@@ -1,6 +1,6 @@
 loadMenu()
 document.getElementById("pullDown").addEventListener("click", catFunction());
-document.getElementById("getButton").addEventListener("click", getData);
+document.getElementById("Button").addEventListener("click", getData);
 // Load XML to memory
 function loadMenu() {
     var xmlhttpCinemas = new XMLHttpRequest();
@@ -112,106 +112,106 @@ function getData() {
     }
 }
 
-    function findMovies() {
-        var x = document.getElementById("moviesearch").value;
-        var xmlhttpSchedule = new XMLHttpRequest();
-        // We replace the statif file with URL
-        //
-        xmlhttpSchedule.open("GET", "https://www.finnkino.fi/xml/Schedule/", true);
-        xmlhttpSchedule.send();
-        xmlhttpSchedule.onreadystatechange = function () {
-            // If we are still loading...
-            if (xmlhttpSchedule.readyState === 1) {
-                document.getElementById("schedules").innerHTML = "Loading...";
-            }
-            // If everything is ok
-            if (xmlhttpSchedule.readyState === 4 && xmlhttpSchedule.status === 200) {
-                // Instead of just placing the RAW XML content we need to parse it
-                //document.getElementById("myDiv").innerHTML = xmlhttp.responseText;
-                //
-                var xmlDocSchedule = xmlhttpSchedule.responseXML;
-                console.log(xmlDocSchedule);
-                // Once again we find some tags from our variable containing XML
-                var items = xmlDocSchedule.getElementsByTagName("Show");
-                // And since they are arrays, we use for loop to travel through them
-                // I wanted to display results in a table so im creating table tags and table cells
-                //
-                //
-                var theathrename, item, content, pic, idSchedule, thea, name, rate, feedlink, description, movies, genre, time = '';
-                for (j = 1; j < items.length; j++) {
-
-                    name = items[j].getElementsByTagName('Title').item(0).firstChild.nodeValue;
-                    if (name == x) {
-                        movies = items[j].getElementsByTagName('Title').item(0).firstChild.nodeValue;
-                        pic = items[j].getElementsByTagName('EventSmallImageLandscape').item(0).firstChild.nodeValue;
-                        rate = items[j].getElementsByTagName('RatingImageUrl').item(0).firstChild.nodeValue;
-                        feedlink = items[j].getElementsByTagName('ShowURL').item(0).firstChild.nodeValue;
-                        genre = items[j].getElementsByTagName('Genres').item(0).firstChild.nodeValue;
-                        time = items[j].getElementsByTagName('dttmShowStart').item(0).firstChild.nodeValue;
-                        theathrename = items[j].getElementsByTagName('Theatre').item(0).firstChild.nodeValue
-                        item = document.getElementById("schedules").innerHTML = '<div id="contentBox" class="gradie"><img class="image" src="' + pic + '"><img class="rate" src="' + rate + '"><a href="' + feedlink + '"><h3>' + movies + '</h3></a>' + '<a href="' + feedlink + '">' + name + '</a>' + '<p>' + '<strong>Teatteri: </strong><br>' + theathrename + '</p>' + '<p>' + '<strong> Näytösaika: </strong><br>' + time + '</p>' + '<p class="genre">' + genre + '</p></div>';
-                        content += item;
-                    } else {
-                        document.getElementById("moviesearch").style.borderColor = "red";
-                    }
-
-                }
-                if (typeof content !== 'undefined') {
-                    document.getElementById("schedules").innerHTML = "<ul>" + content + "</ul>";
-                } else {
-                    console.log("Tyhjää");
-                }
-            }
-
+function findMovies() {
+    var x = document.getElementById("moviesearch").value;
+    var xmlhttpSchedule = new XMLHttpRequest();
+    // We replace the statif file with URL
+    //
+    xmlhttpSchedule.open("GET", "https://www.finnkino.fi/xml/Schedule/", true);
+    xmlhttpSchedule.send();
+    xmlhttpSchedule.onreadystatechange = function () {
+        // If we are still loading...
+        if (xmlhttpSchedule.readyState === 1) {
+            document.getElementById("schedules").innerHTML = "Loading...";
         }
-    }
+        // If everything is ok
+        if (xmlhttpSchedule.readyState === 4 && xmlhttpSchedule.status === 200) {
+            // Instead of just placing the RAW XML content we need to parse it
+            //document.getElementById("myDiv").innerHTML = xmlhttp.responseText;
+            //
+            var xmlDocSchedule = xmlhttpSchedule.responseXML;
+            console.log(xmlDocSchedule);
+            // Once again we find some tags from our variable containing XML
+            var items = xmlDocSchedule.getElementsByTagName("Show");
+            // And since they are arrays, we use for loop to travel through them
+            // I wanted to display results in a table so im creating table tags and table cells
+            //
+            //
+            var theathrename, item, content, pic, idSchedule, thea, name, rate, feedlink, description, movies, genre, time = '';
+            for (j = 1; j < items.length; j++) {
 
-    function loadSchedule(filename) {
-        var x = document.getElementById("schedules");
-        if (x.style.display === "none") {
-            x.style.display = "block";
-        } else {
-            x.style.display = "none";
-        }
-        var xmlhttpSchedule = new XMLHttpRequest();
-        // We replace the statif file with URL
-        //
-        xmlhttpSchedule.open("GET", filename, true);
-        xmlhttpSchedule.send();
-        xmlhttpSchedule.onreadystatechange = function () {
-            // If we are still loading...
-            if (xmlhttpSchedule.readyState === 1) {
-                document.getElementById("schedules").innerHTML = "Loading...";
-            }
-            // If everything is ok
-            if (xmlhttpSchedule.readyState === 4 && xmlhttpSchedule.status === 200) {
-                // Instead of just placing the RAW XML content we need to parse it
-                //document.getElementById("myDiv").innerHTML = xmlhttp.responseText;
-                //
-                var xmlDocSchedule = xmlhttpSchedule.responseXML;
-                console.log(xmlDocSchedule);
-                // Once again we find some tags from our variable containing XML
-                var items = xmlDocSchedule.getElementsByTagName("Show");
-                // And since they are arrays, we use for loop to travel through them
-                // I wanted to display results in a table so im creating table tags and table cells
-                //
-                //
-                var item, content, pic, name = '';
-                for (i = 1; i < items.length; i++) {
-
-                    //pic = items[i].getElementsByTagName('EventSmallImagePortrait')[0].firstChild.nodeValue;
-                    name = items[i].getElementsByTagName('Theatre').item(0).firstChild.nodeValue;
-                    item = "<ul>" + name + "</ul>";
+                name = items[j].getElementsByTagName('Title').item(0).firstChild.nodeValue;
+                if (name.match(x)) {
+                    movies = items[j].getElementsByTagName('Title').item(0).firstChild.nodeValue;
+                    pic = items[j].getElementsByTagName('EventSmallImageLandscape').item(0).firstChild.nodeValue;
+                    rate = items[j].getElementsByTagName('RatingImageUrl').item(0).firstChild.nodeValue;
+                    feedlink = items[j].getElementsByTagName('ShowURL').item(0).firstChild.nodeValue;
+                    genre = items[j].getElementsByTagName('Genres').item(0).firstChild.nodeValue;
+                    time = items[j].getElementsByTagName('dttmShowStart').item(0).firstChild.nodeValue;
+                    theathrename = items[j].getElementsByTagName('Theatre').item(0).firstChild.nodeValue
+                    item = document.getElementById("schedules").innerHTML = '<div id="contentBox" class="gradie"><img class="image" src="' + pic + '"><img class="rate" src="' + rate + '"><a href="' + feedlink + '"><h3>' + movies + '</h3></a>' + '<a href="' + feedlink + '">' + name + '</a>' + '<p>' + '<strong>Teatteri: </strong><br>' + theathrename + '</p>' + '<p>' + '<strong> Näytösaika: </strong><br>' + time + '</p>' + '<p class="genre">' + genre + '</p></div>';
                     content += item;
-                    console.log(content);
+                } else {
+                    document.getElementById("moviesearch").style.borderColor = "red";
                 }
-                // Finally we place the contents in a div
+
+            }
+            if (typeof content !== 'undefined') {
                 document.getElementById("schedules").innerHTML = "<ul>" + content + "</ul>";
+            } else {
+                console.log("Tyhjää");
+            }
+        }
+
+    }
+}
+
+function loadSchedule(filename) {
+    var x = document.getElementById("schedules");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    var xmlhttpSchedule = new XMLHttpRequest();
+    // We replace the statif file with URL
+    //
+    xmlhttpSchedule.open("GET", filename, true);
+    xmlhttpSchedule.send();
+    xmlhttpSchedule.onreadystatechange = function () {
+        // If we are still loading...
+        if (xmlhttpSchedule.readyState === 1) {
+            document.getElementById("schedules").innerHTML = "Loading...";
+        }
+        // If everything is ok
+        if (xmlhttpSchedule.readyState === 4 && xmlhttpSchedule.status === 200) {
+            // Instead of just placing the RAW XML content we need to parse it
+            //document.getElementById("myDiv").innerHTML = xmlhttp.responseText;
+            //
+            var xmlDocSchedule = xmlhttpSchedule.responseXML;
+            console.log(xmlDocSchedule);
+            // Once again we find some tags from our variable containing XML
+            var items = xmlDocSchedule.getElementsByTagName("Show");
+            // And since they are arrays, we use for loop to travel through them
+            // I wanted to display results in a table so im creating table tags and table cells
+            //
+            //
+            var item, content, pic, name = '';
+            for (i = 1; i < items.length; i++) {
+
+                //pic = items[i].getElementsByTagName('EventSmallImagePortrait')[0].firstChild.nodeValue;
+                name = items[i].getElementsByTagName('Theatre').item(0).firstChild.nodeValue;
+                item = "<ul>" + name + "</ul>";
+                content += item;
                 console.log(content);
             }
+            // Finally we place the contents in a div
+            document.getElementById("schedules").innerHTML = "<ul>" + content + "</ul>";
+            console.log(content);
         }
     }
+}
 
-    function catFunction() {
-        console.log("kissa");
-    }
+function catFunction() {
+    console.log("kissa");
+}
